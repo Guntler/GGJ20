1743e6aec692943ebfffa9b2ce0a8183ac47a73b
João Marinheiro <joca.mar2011@gmail.com>
2020-02-01 16:15:19 +0000

    Added starting classes

---
+++ GGJ20Game/Assets/SongManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SongManager : MonoBehaviour
+{
+    public double LowerLimit;
+    public double UpperLimit;
+
+    private List<GameObject> notes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        notes = new List<GameObject>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+    }
+}
