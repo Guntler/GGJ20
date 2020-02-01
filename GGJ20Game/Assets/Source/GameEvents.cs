a6b0218e73fccc84190407b48df262f80e370164
Guntler <pbpdirector@hotmail.com>
2020-02-01 11:38:48 +0000

    First commit.

---
+++ GGJ20Game/Assets/Source/GameEvents.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Update100Evt : GameEvent
+{
+
+}
+
+public class Update200Evt : GameEvent
+{
+
+}
+
+public class Update500Evt : GameEvent
+{
+
+}
+
+public class Update1000Evt : GameEvent
+{
+
+}
+
+public class GameEndEvt : GameEvent
+{
+
+}
+
+public class GameStartEvt : GameEvent
+{
+
+}
+
+public class SpawnRocketEvt : GameEvent
+{
+
+}
